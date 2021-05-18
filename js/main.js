92afcf96efa90c623665db29976cf08ca69fc472
pagarevijayy <pagarevj97@gmail.com>
2021-05-19 03:34:31 +0530

    hero-section

---
+++ js/main.js
@@ -0,0 +1,23 @@
+console.log('javascript connected!')
+
+const hamburgerIcon = document.getElementById('hamburgerIcon');
+const mobileNav = document.getElementById('mobileNav');
+
+function toggleHamburgerMenu() {
+    const isActive = "is-active";
+
+    if (hamburgerIcon.classList.contains(isActive)) {
+        // close nav menu
+        hamburgerIcon.classList.remove(isActive);
+        mobileNav.classList.add('hidden');
+        mobileNav.classList.remove('block');
+
+
+    } else {
+        // open nav menu
+        hamburgerIcon.classList.add(isActive);
+        mobileNav.classList.add('block');
+        mobileNav.classList.remove('hidden');
+
+    }
+}
